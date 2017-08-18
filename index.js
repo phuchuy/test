745ec981ace64da3b99379e6821d54ab34f1afee
Huy Huynh <huy.huynh@123rf.com>
2017-08-18 17:58:16 +0800

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
